93ce05349483981cbf0fe5b0f8b2dd2828904a5d
dnsmkl <danas.mikelinskas@gmail.com>
2012-04-11 13:36:09 +0300

    add short sql sample for testing

---
+++ testing/sample1.sql
@@ -0,0 +1,11 @@
+select asbaba as g 
+, cast( bfbfb as date) +1 as x
+, min(a) over (partition by x,c order by r rows between 1 preceeding and 1 preceeding)
+,'dfgdf/*gdfgd*/fg''dfgdf/*gdfgd*/fg''dfgdf/*gdfgd*/fg' as c
+, cast(19.2 as decimal(18,2))
+, ' */ '
+, cast(   19.2   as  decimal( 18   ,   2     )   )
+/* sdsd  *****---*/
+
+,asextract(day from t) as _toto -- komentaras case select from
+from (bobo    as gogo ) t 
