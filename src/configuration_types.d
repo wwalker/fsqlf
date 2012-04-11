9305b86e5f6c168a0c3290ff8746619844dc6782
dnsmkl <danas.mikelinskas@gmail.com>
2012-04-11 13:21:59 +0300

    Indentation changed from 3 spaces to 4

--- src/configuration_types.d
+++ src/configuration_types.d
@@ -3,42 +3,43 @@ module configuration_types;
 
 struct Spacing
 {
-   int newLines=0;
-   int tabs=0;
-   int spaces=0;
+    int newLines=0;
+    int tabs=0;
+    int spaces=0;
 
-   string tab="    ";
-   pure auto outputString()
-   {
-      string result="";
-      for(int i=0 ; i < newLines ; i++) result ~= "\n";
-      for(int i=0 ; i < tabs     ; i++) result ~= tab;
-      for(int i=0 ; i < spaces   ; i++) result ~= " ";
-      return result;
-   }
+    string tab="    ";
+    pure auto outputString()
+    {
+        string result="";
+        for(int i=0 ; i < newLines ; i++) result ~= "\n";
+        for(int i=0 ; i < tabs     ; i++) result ~= tab;
+        for(int i=0 ; i < spaces   ; i++) result ~= " ";
+        return result;
+    }
 
-   unittest
-   {
-      assert(Spacing(2,1,3).outputString() == "\n\n       ");
-      assert(Spacing(1,1,1).outputString() == "\n     ");
-      assert(Spacing(0,2,1).outputString() == "         ");
-      assert(Spacing(0,0,0).outputString() == "");
-   }
+    unittest
+    {
+        assert(Spacing(2,1,3).outputString() == "\n\n       ");
+        assert(Spacing(1,1,1).outputString() == "\n     ");
+        assert(Spacing(0,2,1).outputString() == "         ");
+        assert(Spacing(0,0,0).outputString() == "");
+    }
 }
 
 
 
 struct Keyword
 {
-   import std.regex;
-   string optionNameShort;
-   Spacing spacingBefore,spacingAfter;
-   string textShort, textLong;
-   bool mandatorySpacing;
-   string regexStr;
+    import std.regex;
+    string optionNameShort;
+    Spacing spacingBefore,spacingAfter;
+    string textShort, textLong;
+    bool mandatorySpacing;
+    string regexStr;
 
-   auto initRegex(){ this.patern = regex("^" ~ this.textLong,"i"); }
-   auto match(string txt){ return std.regex.match(txt , this.patern); }
-   private:
-   Regex!char patern;
+    auto initRegex(){ this.patern = regex("^" ~ this.textLong,"i"); }
+    auto match(string txt){ return std.regex.match(txt , this.patern); }
+
+    private:
+    Regex!char patern;
 }
--- src/fsqlf.d
+++ src/fsqlf.d
@@ -7,20 +7,20 @@ import std.stdio;
 
 void format_sql(Keyword[string] k, string input, File output=std.stdio.stdout)
 {
-   string input_text = read_input(input);
-   auto tokens_separate = lex(input_text, k);   // split text into words, puntation/space chars and comments
-   auto keywords = combine(tokens_separate); // recognise logical keywords like 'LEFT OUTER JOIN'; Also handle such cases as LEFT /*f */ JOIN
-   auto kw_spaced = space_insert(keywords);  // insert spaces simply by looking at the keywords
-   auto kw_formed = space_adjust(kw_spaced); // adjust spacing by context
-   auto text_formed = toString(kw_formed);   // convert inner structure to string
-   write_output(text_formed, output);        // write to the output
+    string input_text = read_input(input);
+    auto tokens_separate = lex(input_text, k); // split text into words, puntation/space chars and comments
+    auto keywords = combine(tokens_separate);  // recognise logical keywords like 'LEFT OUTER JOIN'; Also handle such cases as LEFT /*f */ JOIN
+    auto kw_spaced = space_insert(keywords);   // insert spaces simply by looking at the keywords
+    auto kw_formed = space_adjust(kw_spaced);  // adjust spacing by context
+    auto text_formed = toString(kw_formed);    // convert inner structure to string
+    write_output(text_formed, output);         // write to the output
 }
 
 auto read_input(in string input) { return input; }
 auto lex(in string input, Keyword[string])
 {
-   
-   return input;
+
+    return input;
 }
 auto combine(in string input) { return input; }
 auto space_insert(in string input) { return input; }
@@ -31,6 +31,6 @@ void write_output(in string txt, File output_stream) { output_stream.writefln(tx
 void main()
 {
 
-   format_sql(keyword_conf.k, "SELECT 1 as x FROM gsd");
-   //foreach(x;k){writeln(x.textShort);}
+    format_sql(keyword_conf.k, "SELECT 1 as x FROM gsd");
+    //foreach(x;k){writeln(x.textShort);}
 }
